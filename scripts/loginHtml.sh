366e2e11839cce806227d753c5e96f17101afbfc
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-09-11 19:02:23 +0000

    htmx

--- scripts/loginHtml.sh
+++ scripts/loginHtml.sh
@@ -2,6 +2,7 @@
 
 curl -X POST http://localhost:9090/superuser/login \
 -H "Content-Type: application/json" \
+-H "Accept": "text/html" \
 -d '{
   "email": "testuser@example.com",
   "password": "password123"
--- scripts/registerHtml.sh
+++ scripts/registerHtml.sh
@@ -2,6 +2,7 @@
 
 curl -X POST http://localhost:9090/superuser/register \
 -H "Content-Type: application/json" \
+-H "Accept": "text/html" \
 -d '{
   "username": "testuser",
   "email": "testuser2@example.com",
