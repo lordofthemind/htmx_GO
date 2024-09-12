b9e57d0bba4a476112bd227783c3b5dfc7b64598
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-09-12 15:35:07 +0000

    hehe

--- internals/types/SuperUserType.go
+++ internals/types/SuperUserType.go
@@ -7,6 +7,7 @@ type Superuser struct {
 	Username  string             `bson:"username" json:"username" validate:"required,min=3,max=32"`
 	Email     string             `bson:"email" json:"email" validate:"required,email"`
 	Password  string             `bson:"password" json:"password" validate:"required,min=6"`
+	Role      string             `bson:"role" json:"role" validate:"required"`
 	CreatedAt int64              `bson:"created_at" json:"created_at"`
 	UpdatedAt int64              `bson:"updated_at" json:"updated_at"`
 }
